ce9fcff4627f639fcb17bca4a9520a31403eb13a
VirLg <19virlg@gmail.com>
2023-07-20 00:14:16 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import ModalWindow from './Modal/Modal';
+import Searchbar from './Searchbar/Searchbar';
+
+class App extends Component {
+  state = {
+    showModal: false,
+  };
+
+  getRequestSearch = data => {
+    console.log(data);
+  };
+  togleShowModal = () => {
+    this.setState({ showModal: !this.state.showModal });
+  };
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <Searchbar getSearch={this.getRequestSearch} />
+        {this.state.showModal && <ModalWindow></ModalWindow>}
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,10 @@
+const ModalWindow = () => {
+  return (
+    <div class="overlay">
+      <div class="modal">
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+};
+export default ModalWindow;
