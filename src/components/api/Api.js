744c93a03cf69524aa79bd3f0fd8d37979b9420c
VirLg <108211942+VirLg@users.noreply.github.com>
2023-07-24 20:17:48 +0300

    add

--- src/components/api/Api.js
+++ src/components/api/Api.js
@@ -5,7 +5,7 @@ const Api = async value => {
   // const fetchCard = fetch(`https://pixabay.com/api/?key=34368263-756a5eb3a3e360b335b61bac8`).then(resp=>resp.json()).then(data=>{return data})
 
   const data = axios(
-    `?key=34368263-756a5eb3a3e360b335b61bac8&q={value}`
+    `?key=34368263-756a5eb3a3e360b335b61bac8&q={value}&per_page=12&page=1`
   );
   return data;
 };
