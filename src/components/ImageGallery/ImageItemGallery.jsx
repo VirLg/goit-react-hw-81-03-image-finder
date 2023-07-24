c6953ea4ae3bd2127dc4b81d274d07c067653fae
VirLg <108211942+VirLg@users.noreply.github.com>
2023-07-24 22:41:36 +0300

    add

--- src/components/ImageGallery/ImageItemGallery.jsx
+++ src/components/ImageGallery/ImageItemGallery.jsx
@@ -2,7 +2,7 @@ const ImageItemGallery = ({ value }) => {
   console.log(value);
   return value.map(({ id, pageURL, previewURL, user }) => {
     return (
-      <li key={id}>
+      <li key={id} onClick={() => console.log(id)}>
         <img src={previewURL} alt={user} />
       </li>
     );
--- src/components/api/Api.js
+++ src/components/api/Api.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 const Api = async value => {
-  // console.log(value);
   // const fetchCard = fetch(`https://pixabay.com/api/?key=34368263-756a5eb3a3e360b335b61bac8`).then(resp=>resp.json()).then(data=>{return data})
 
   const data = axios(
-    `?q=${value}&key=34368263-756a5eb3a3e360b335b61bac8`
+    `?q=${value}&key=34368263-756a5eb3a3e360b335b61bac8&image_type=photo&orientation=horizontal&per_page=12&page=1`
   );
   return data;
 };
+
 export default Api;
 // q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12
-// &image_type=photo&orientation=horizontal&per_page=12&page=1
+//
